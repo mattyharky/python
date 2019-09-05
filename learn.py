c8dfd85070c4913be51ac85011ebda8bd25c4215
mattyharky <mattyharky@gmail.com>
2019-09-05 21:12:46 +0100

    adding lesson

--- learn.py
+++ learn.py
@@ -11,4 +11,18 @@ of a multi-line comment.
 You can type in what ever you like and each line will be treated as a comment.
 The multi-line comment ends when another set of three double quote marks are typed.
 """
+#A variable is a place in computer memory that can hold and remember information
+#Your brain's memory has variables as well
+#Examples are your name, your age, your address
+#Below are examples of variables in a computers memory
+sMyForename = "Matthew"
+sMySurname = "Harkin"
+sMyNickname = "Matty"
+sMyGamerTag = "Glassjaw"
+#All of these variables have a small s at the start.
+#This means they hold strings of text.
+#Notice that all of the text after "  contain strings of text data
+sMyAddress = "10 Main Street, Dumfries, DG2 7PR"
+#The example above has lots of string of text data
+#You are allowed to mix numbers with text but cannot do arithmetic on this
 

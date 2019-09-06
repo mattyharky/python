f07153a6f5703d743a90da5b86fdd1ed82f3f0c0
mattyharky <mattyharky@gmail.com>
2019-09-06 10:21:14 +0100

    Arithmetic

--- learn.py
+++ learn.py
@@ -62,3 +62,41 @@ print(sMyGamerTag + " is my gamertag")
 print("I am " + str(iAge) + " years old")
 print("I was born in " + str(iYearBorn) + " and my shoe size is " + str(fMyShoeSize) + " UK")
 
+#You can do arithmetic in code
+#+ is to add
+#- is to subtract
+#* is to multiply
+#/ is to divide
+#% is to do modulus or remainder
+#() anything within these are worked out first
+#BDMAS is used Brackets, Division, Multiplication, Addition, Subtraction
+iTotal = 0 + 1 + 2 + 3 + 4 + 5
+print(iTotal)
+iTotal = 20 + 20
+print(iTotal)
+iTotal = 100 - 10 - 5
+print(iTotal)
+iTotal = 20 + 20 - 10
+print(iTotal)
+iTotal = iTotal * 2
+print(iTotal)
+iTotal = iTotal / 10
+print(iTotal)
+iTotal = 100 / 9
+print(iTotal)
+#The value is suddenly floating point
+iTotal = int(iTotal)
+#The int() command turns a floating point into an integer
+print(iTotal)
+iTotal = 100 % 9
+print(iTotal)
+#Here the remainder of 100 / 9 is shown
+iTotal = int((20 - 10) + ((20 / 5) * 2))
+print(iTotal)
+#As you can see we can do complex looking arithmetic
+fAppleCost = 0.55
+fPearCost = 0.60
+fFruitCost = fAppleCost + fPearCost
+print("The fruit cost £" + str(fFruitCost))
+#This example uses arithmetic by adding two variables
+
